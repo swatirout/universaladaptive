bbf3bbcfab7d14417f2a0f1e34634f6d46470009
Swati Rout <swatirout@macbook-pro.local>
2019-01-21 16:01:26 +0530

    Initial Commit

---
+++ universaladaptive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  universaladaptive
+//
+//  Created by Swati Rout on 21/01/19.
+//  Copyright © 2019 Swati Rout. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
